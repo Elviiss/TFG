92926b38a64aa354b58e830c2b559e54c2e3d476
Elviiss <elvis22elmejor@gmail.com>
2023-11-24 04:18:07 +0100

    creada y enlazada la vista de los detalles de cada producto

--- routes/mainrouter.js
+++ routes/mainrouter.js
@@ -5,18 +5,17 @@ const mainrouter = Router()
 
 mainrouter.get("/", async (req,res) => {
     console.log(await pool.query('SELECT * FROM zapa'))
-
     const [zapass] = await pool.query('SELECT * FROM zapa')
     console.log(zapass)
-    console.log(zapass.id)
-
     res.render("main", {zapass})
 
 } )
 
-
-mainrouter.get("/vistazapas",(req,res) => {
-    res.render("vistazapas")
+mainrouter.get("/vistazapas/:id", async(req,res) => {
+    const [zapasss] = await pool.query('SELECT * from zapa where id = ?', [req.params.id])
+    console.log(zapasss)
+    console.log(req.params.id)
+    res.render("vistazapas", {zapasss})
 } )
 
 
