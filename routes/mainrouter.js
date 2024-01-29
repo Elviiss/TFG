eef87d51ef9dfe2b7ffd113fba133dc27ac0dd0a
Elviiss <elvis22elmejor@gmail.com>
2024-01-29 15:22:15 +0100

    cambiando los diseños y demás

--- routes/mainrouter.js
+++ routes/mainrouter.js
@@ -4,20 +4,14 @@ const mainrouter = Router()
 
 
 mainrouter.get("/", async (req,res) => {
-    console.log(await pool.query('SELECT * FROM zapa'))
     const [zapass] = await pool.query('SELECT * FROM zapa')
-    console.log(await pool.query('SELECT * FROM marcaprinci'))
     const [marcaprincii] = await pool.query('SELECT * FROM marcaprinci')
-    console.log(zapass)
-    console.log(marcaprincii)
     res.render("main", {zapass,marcaprincii})
 
 } )
 
 mainrouter.get("/vistazapas/:id", async(req,res) => {
     const [zapasss] = await pool.query('SELECT * from zapa where id = ?', [req.params.id])
-    console.log(zapasss)
-    console.log(req.params.id)
     res.render("vistazapas", {zapasss})
 } )
 
@@ -29,7 +23,6 @@ mainrouter.get("/vermaszapas", async (req,res) => {
 mainrouter.get("/vermasmarcas/:marca", async (req,res) => { 
     const [zapass] = await pool.query('SELECT * FROM zapa where marca = ?', [req.params.marca])
     const [marcaprincii] = await pool.query('SELECT * FROM marcaprinci where marca = ?', [req.params.marca])
-    console.log(req.params.marca)
     res.render("vermasmarcas", {zapass,marcaprincii})
 } )
 
