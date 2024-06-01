d74e5aa0ac5b66f1e7391f04b518290ea0bc088d
Elviiss <elvis22elmejor@gmail.com>
2024-06-02 01:22:41 +0200

    desplegada la base de datos

--- keys.js
+++ keys.js
@@ -8,5 +8,12 @@ module.exports = {
         password: 'root',
         database: 'flaffy'
     },
+    databasenube:{
+        host: 'blh6v2r6gpkyta1jfa6a-mysql.services.clever-cloud.com',
+        user: 'u857enpsnmyh83no',
+        password: '7DbGwc0adPvpUwb4mLet',
+        port: 3306,
+        database: 'blh6v2r6gpkyta1jfa6a'
+    },
     STRIPE_PRIVATE_KEY: process.env.STRIPE_PRIVATE_KEY
 }
